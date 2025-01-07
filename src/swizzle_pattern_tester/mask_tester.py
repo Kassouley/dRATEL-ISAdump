005e45154076a8cf1bb941c84574b34d65a5e2ed
Kass <lucas.real.neto@gmail.com>
2025-01-07 15:29:32 +0100

    fix swizzle tester

--- src/swizzle_pattern_tester/mask_tester.py
+++ src/swizzle_pattern_tester/mask_tester.py
@@ -1,3 +1,6 @@
+import re
+import subprocess
+
 symbols = ['i', 'p', '1', '0']
 def generate_combinations(current, length, symbols, results):
     if length == 0:
@@ -26,5 +29,36 @@ asm = "\\n \\\n".join(asm_lines)
 
 source = source + asm + '");\n}'
 
-with open("../../output/swizzle_pattern_tester/mask_tester.cpp", "w") as file:
+
+output_file = "mask_tester.cpp"
+with open(output_file, "w") as file:
     file.write(source)
+
+result = subprocess.run("hipcc " + output_file, shell=True, text=True, capture_output=True)
+
+if result.returncode != 0: 
+    print("Command Error:")
+    print(result.stderr)
+    exit()
+
+result = subprocess.run('roc-obj -d a.out && grep -v "s_nop" a.out\:1.hipv4-amdgcn-amd-amdhsa--gfx90a.s', shell=True, text=True, capture_output=True)
+
+print(result.stdout)  # Standard output of the command
+
+if result.returncode != 0: 
+    print("Command Error:")
+    print(result.stderr)
+    exit()
+
+pattern = r'(swizzle\(.*?\)) // [0-9A-F]*: ([0-9A-F]{8})'
+
+for line in result.stdout.splitlines():
+    match = re.search(pattern, line, re.IGNORECASE)
+    if match:
+        swizzle_part = match.group(1)
+        hex_code = match.group(2)  # First hex code after the colon
+        last_4_chars = hex_code[-4:]
+        binary_equivalent = bin(int(last_4_chars, 16))[2:].zfill(16)  # Convert to binary and pad to 16 bits
+        spaced_binary = ' '.join([binary_equivalent[i:i+4] for i in range(0, len(binary_equivalent), 4)])
+        print(f"{swizzle_part:<{40}} : {last_4_chars:<{5}} : {spaced_binary:<{20}}")
+
