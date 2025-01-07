20ac3025e42d56c579a7b8390b33c2c0a4b456e8
Kass <lucas.real.neto@gmail.com>
2025-01-07 15:06:50 +0100

    fix swizzle tester

--- src/swizzle_pattern_tester/mask_tester.py
+++ src/swizzle_pattern_tester/mask_tester.py
@@ -16,22 +16,6 @@ int main(void) { return 0; }
 
 __global__ void ds_swizzle_b32() {
     __asm__ volatile("
-        ds_swizzle_b32 v0, v0 offset:swizzle(QUAD_PERM, 0, 0, 0, 0)   \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(QUAD_PERM, 0, 1, 2, 3)   \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(QUAD_PERM, 3, 3, 3, 3)   \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(BITMASK_PERM, \"00000\") \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(BITMASK_PERM, \"11111\") \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(BITMASK_PERM, \"iiiii\") \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(BITMASK_PERM, \"ppppp\") \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(BITMASK_PERM, \"01pi0\") \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(BROADCAST, 2, 0)         \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(BROADCAST, 32, 0)        \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(SWAP, 1)                 \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(SWAP, 8)                 \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(SWAP, 16)                \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(REVERSE, 2)              \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(REVERSE, 16)             \n \\
-                      ds_swizzle_b32 v0, v0 offset:swizzle(REVERSE, 32)             \n");
 }
 """
 
@@ -43,5 +27,5 @@ asm = "\n".join(asm_lines)
 
 source = source + asm + '\n");\n}'
 
-with open("swizzle_pattern_tester/mask_tester.cpp", "w") as file:
+with open("../../output/swizzle_pattern_tester/mask_tester.cpp", "w") as file:
     file.write(source)
